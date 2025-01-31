1f2c367a9f32454eea90330d66ee2f131cb429d0
karishan <karishanshandrakumar@gmail.com>
2025-01-31 17:40:52 +0000

    improved new route to showing time stamps of different instruments

--- app/utils/predict_instruments.py
+++ app/utils/predict_instruments.py
@@ -9,8 +9,41 @@ instrument_names = [
     "violin", "voice"
 ]
 
+def smooth_timestamps(time_stamps, min_duration = 2):
+    if not time_stamps:
+        return []
+    
+    filtered = []
+    temp_group = [time_stamps[0]]
 
-def predict_instruments(embeddings: np.ndarray, threshold=0.5, time_step=1) -> list:
+    for i in range(1, len(time_stamps)):
+        if time_stamps[i] == time_stamps[i - 1] + 1:
+            temp_group.append(time_stamps[i])
+        else:
+            if len(temp_group) >= min_duration:
+                filtered.extend(temp_group)
+            temp_group = [time_stamps[i]]
+
+    if len(temp_group) >= min_duration:
+        filtered.extend(temp_group)
+
+    return filtered
+
+def get_segment_starts(active_time_stamps):
+
+    if not active_time_stamps:
+        return []
+    
+    segment_starts = [active_time_stamps[0]]
+
+    for i in range(1, len(active_time_stamps)):
+        if active_time_stamps[i] >= active_time_stamps[i - 1] + 3:  # If there's a gap, start a new segment
+            segment_starts.append(active_time_stamps[i])
+
+    return segment_starts
+    
+
+def predict_instruments(embeddings: np.ndarray, threshold=0.2, time_step=1) -> list:
 
     model = TensorflowPredict2D(
         graphFilename="models/mtg_jamendo_instrument-discogs-effnet-1.pb"
@@ -34,6 +67,10 @@ def predict_instruments(embeddings: np.ndarray, threshold=0.5, time_step=1) -> l
             if probability >= threshold:
                 time_stamp = round(float(t * time_step), 2)  
                 active_timestamps.append(time_stamp)
+        
+        active_timestamps = smooth_timestamps(active_timestamps, min_duration=3)
+        active_timestamps = get_segment_starts(active_timestamps)
+
 
         # Only includes instruments that has the active timestamps
         if active_timestamps:
